0e2e419cf540793e5f961e2d3d05b492a7e1559a
michelle <michelleaklufi@gmail.com>
2022-08-02 10:51:55 -0400

    camera stuff + tab view controller

---
+++ giftly/PhotoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  giftly
+//
+//  Created by Scholar on 8/1/22.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController ()
+
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+
+   
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+            }
+    
+    @IBAction func takeSelfieTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+            }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageDisplay.image = selectedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    }
+    
+
